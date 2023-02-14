0d296520ce321714beb607337f8245ae9650e07c
HiHi3690 <jaksonduimstra@gmail.com>
2023-02-13 23:41:42 -0500

    implemented Pigeon2Accelerometer and more SOK changes

--- src/main/java/frc/robot/util/SecondOrderKinematics.java
+++ src/main/java/frc/robot/util/SecondOrderKinematics.java
@@ -1,37 +1,57 @@
 package frc.robot.util;
 
+import com.ctre.phoenix.sensors.Pigeon2;
+
 import edu.wpi.first.math.kinematics.SwerveDriveKinematics;
+import frc.robot.Constants.DriveConstants;
 
 public class SecondOrderKinematics extends SwerveDriveKinematics {
+
+  Pigeon2 m_Gyro = new Pigeon2(DriveConstants.kGyroPort);
+  Pigeon2Accelerometer m_Accelerometer = new Pigeon2Accelerometer(m_Gyro);
+
+  double[] accelXYT = m_Accelerometer.getAccelMetersPerSecond();
+  double accelX = accelXYT[0];
+  double accelY = accelXYT[0];
+
+  private double getBotAccel() {
+    return Math.sqrt(accelX * accelX + accelY * accelY);
+  }
+
+  private double getThetaMRobot() {
+    //Change to get module angle - robot angle later
+    return 0;
+  }
+
+  private double getThetaVelMRobot() {
+    //Change to get module angle vel - robot angle vel later
+    return 0;
+  }
 
   /**
   * Get a module's X Acceleration component
   * @param botVel bot's velocity
-  * @param botAccel bot's acceleration
   * @param thetaMRobot should be thetaModule - thetaRobot
   * @param thetaVelMRobot should be thetaVelModule - thetaVelRobot
   */
-  public static double getModulePositionAccelX(double botVel,
-      double botAccel,
+  public double getModulePositionAccelX(double botVel,
       double thetaMRobot,
       double thetaVelMRobot) {
-    double Amx = botAccel * Math.cos(thetaMRobot) - botVel * thetaVelMRobot * Math.sin(thetaMRobot);
-    return Amx;
+    double A_mx = getBotAccel() * Math.cos(thetaMRobot) - botVel * thetaVelMRobot * Math.sin(thetaMRobot);
+    return A_mx;
   }
 
   /**
   * Get a module's Y Acceleration component
   * @param botVel bot's velocity
-  * @param botAccel bot's acceleration
   * @param thetaMRobot should be thetaModule - thetaRobot
   * @param thetaVelMRobot should be thetaVelModuule - thetaVelRobot
   */
-  public static double getModulePositionAccelY(double botVel,
-      double botAccel,
+  public double getModulePositionAccelY(double botVel,
       double thetaMRobot,
       double thetaVelMRobot) {
-    double Amy = botAccel * Math.sin(thetaMRobot) + botVel * thetaVelMRobot * Math.cos(thetaMRobot);
-    return Amy;
+    double A_my = getBotAccel() * Math.sin(thetaMRobot) + botVel * thetaVelMRobot * Math.cos(thetaMRobot);
+    return A_my;
   }
 
 }
