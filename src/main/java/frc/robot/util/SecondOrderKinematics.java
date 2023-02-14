d2a46d359f6788d737affe17467ef85340d66afc
HiHi3690 <duimstrajakson@gmail.com>
2023-02-13 21:55:33 -0500

    Added getModuleAccelX/Y assuming inputs

--- src/main/java/frc/robot/util/SecondOrderKinematics.java
+++ src/main/java/frc/robot/util/SecondOrderKinematics.java
@@ -1,31 +1,22 @@
 package frc.robot.util;
 
-import java.util.function.Supplier;
-
-import edu.wpi.first.math.controller.PIDController;
-import edu.wpi.first.math.geometry.Pose2d;
-import edu.wpi.first.math.geometry.Rotation2d;
-import edu.wpi.first.math.kinematics.ChassisSpeeds;
 import edu.wpi.first.math.kinematics.SwerveDriveKinematics;
 
-import com.ctre.phoenix.sensors.WPI_Pigeon2;
-import frc.robot.Constants.DriveConstants;
+public class SecondOrderKinematics extends SwerveDriveKinematics {
 
-public class SecondOrderKinematics extends SwerveDriveKinematics{
-
-  double botVelocity = 0;
-  double botAccel = 0;
-  double thetaMRobot = 0;
-  double thetaAccelMRobot = 0;
-  
-
-  public double getModulePositionAccelX(){
-    double Amx = Math.cos(thetaMRobot)- Math.sin(thetaMRobot);
+  public double getModulePositionAccelX(double botVel,
+      double botAccel,
+      double thetaMRobot,
+      double thetaAccelMRobot) {
+    double Amx = botAccel * Math.cos(thetaMRobot) - botVel * thetaAccelMRobot * Math.sin(thetaMRobot);
     return Amx;
   }
 
-  public double getModulePositionAccelY(){
-    double Amy = Math.sin(thetaMRobot) + Math.cos(thetaMRobot);
+  public double getModulePositionAccelY(double botVel,
+      double botAccel,
+      double thetaMRobot,
+      double thetaAccelMRobot) {
+    double Amy = botAccel * Math.sin(thetaMRobot) + botVel * thetaAccelMRobot * Math.cos(thetaMRobot);
     return Amy;
   }
 
