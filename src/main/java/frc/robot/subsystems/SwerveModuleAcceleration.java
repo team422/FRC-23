5654ff06b2e1afffb9dae4420c39d01293175315
HiHi3690 <jaksonduimstra@gmail.com>
2023-02-16 00:01:29 -0500

    SOK should mostly be done Just need to find bot vel and angVel

---
+++ src/main/java/frc/robot/subsystems/SwerveModuleAcceleration.java
@@ -0,0 +1,57 @@
+package frc.robot.subsystems;
+
+import java.util.Objects;
+
+import edu.wpi.first.math.kinematics.SwerveModuleState;
+
+public class SwerveModuleAcceleration implements Comparable<SwerveModuleAcceleration> {
+  public double accelMetersPerSecondSquared = 0;
+
+  /**
+   * Constructs a SwerveModulePosition.
+   *
+   * @param accelMetersPerSecondSquared The accleration measured by the wheel of the module.
+   */
+  public SwerveModuleAcceleration() {
+  }
+
+  public SwerveModuleAcceleration(double accelMetersPerSecondSquared) {
+    this.accelMetersPerSecondSquared = accelMetersPerSecondSquared;
+  }
+
+  @Override
+  public int hashCode() {
+    return Objects.hash(accelMetersPerSecondSquared);
+  }
+
+  @Override
+  public boolean equals(Object obj) {
+    if (obj instanceof SwerveModuleAcceleration) {
+      SwerveModuleAcceleration other = (SwerveModuleAcceleration) obj;
+      return Math.abs(other.accelMetersPerSecondSquared - accelMetersPerSecondSquared) < 1E-9;
+    }
+    return false;
+  }
+
+  /**
+   * Compares two swerve module accelerations. One swerve module is "greater" than the other if its
+   * acceleration is higher than the other.
+   *
+   * @param other The other swerve module.
+   * @return 1 if this is greater, 0 if both are equal, -1 if other is greater.
+   */
+  @Override
+  public int compareTo(SwerveModuleAcceleration other) {
+    return Double.compare(this.accelMetersPerSecondSquared, other.accelMetersPerSecondSquared);
+  }
+
+  @Override
+  public String toString() {
+    return String.format(
+        "SwerveModuleAccel(Accel: %.2f m)", accelMetersPerSecondSquared);
+  }
+
+  public static SwerveModuleAcceleration calculate(SwerveModuleState current, SwerveModuleState previous) {
+    return new SwerveModuleAcceleration(current.speedMetersPerSecond - previous.speedMetersPerSecond);
+  }
+}
