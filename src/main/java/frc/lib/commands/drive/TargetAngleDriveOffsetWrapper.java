9a4ea381f8aa9a4bca2db98ac6ba3b813b4c0b63
Nick Hadley <nick.hadley31@gmail.com>
2023-03-01 10:40:31 -0500

    Change offset wrapper to supplier

--- src/main/java/frc/lib/commands/drive/TargetAngleDriveOffsetWrapper.java
+++ src/main/java/frc/lib/commands/drive/TargetAngleDriveOffsetWrapper.java
@@ -1,17 +1,23 @@
 package frc.lib.commands.drive;
 
+import java.util.function.Supplier;
+
 import edu.wpi.first.math.geometry.Rotation2d;
 
 public class TargetAngleDriveOffsetWrapper extends TargetAngleDriveWrapper {
-  private final Rotation2d m_offset;
+  private final Supplier<Rotation2d> m_offset;
 
   public TargetAngleDriveOffsetWrapper(TargetAngleDrive targetAngleDrive, Rotation2d offset) {
+    this(targetAngleDrive, () -> offset);
+  }
+
+  public TargetAngleDriveOffsetWrapper(TargetAngleDrive targetAngleDrive, Supplier<Rotation2d> offset) {
     super(targetAngleDrive);
     m_offset = offset;
   }
 
   @Override
   protected Rotation2d getDesiredAngle() {
-    return super.getDesiredAngle().plus(m_offset);
+    return super.getDesiredAngle().plus(m_offset.get());
   }
 }
