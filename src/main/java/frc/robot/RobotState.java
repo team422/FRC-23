06251af04f76e057026db5e7fe88aa74ab72a8c7
Shorya Malani <shoryamal@gmail.com>
2023-04-07 17:01:42 -0400

    elevator

--- src/main/java/frc/robot/RobotState.java
+++ src/main/java/frc/robot/RobotState.java
@@ -303,7 +303,7 @@ public class RobotState {
 
   public Command setpointCommandSequential(double[] setpoint) {
     return Commands.sequence(
-        m_wrist.setAngleCommand(Rotation2d.fromDegrees(75)),
+        m_wrist.setAngleCommand(Rotation2d.fromDegrees(75)).withTimeout(1),
         m_elevator.testSetHeightCommand(setpoint[0], Units.inchesToMeters(46)),
         m_wrist.setAngleCommand(Rotation2d.fromDegrees(setpoint[1])));
   }
